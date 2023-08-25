b2be5c917647390e5e2bdd5756034e03274376a8
haroldosales <haroldognu@gmail.com>
2023-08-25 20:28:03 -0300

    up

---
+++ lessons/lesson4.js
@@ -0,0 +1,14 @@
+// Relational or comparison operators
+
+// > - more than
+// < - less than
+// >= - more than
+// <= less than
+
+var result =  6 <=5
+console.log(result);
+
+//equalit operatos
+var x =1
+console.log(x == '1'); // lose comparion
+console.log(x === '1'); // strict comparison
---
+++ lessons/lesson5.js
@@ -0,0 +1,17 @@
+//logical "AND"
+//console.log( true && false); // all values ahave to be TRUE for expression to be eTRUE
+
+//LOGICAL"OR"
+//console.log(true || false); // any value should be True for thte experession to be True
+
+
+var ageIsMOreThanEigthtenn = false
+var isUSCitize = true
+
+var eligbilityForDriversLicense = ageIsMOreThanEigthtenn || isUSCitize
+console.log(eligbilityForDriversLicense);
+
+
+//Logical "NOT"
+console.log(!true);
+console.log(6 !== 10);
